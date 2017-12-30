b93dc0beaa9f8f9b5d01cab4b09107c49beae013
Miha Markič <miha@rthand.com>
2017-12-30 20:14:53 +0100

    Adds empty GettingStarted project

---
+++ samples/GettingStarted/GettingStarted/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GettingStarted
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
