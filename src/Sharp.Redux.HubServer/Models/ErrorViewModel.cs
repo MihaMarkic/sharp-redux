56dbbe03d52283508d0e66e894306e27cafcd591
Miha Markič <miha@rthand.com>
2018-04-04 11:14:49 +0200

    Adds server

---
+++ src/Sharp.Redux.HubServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sharp.Redux.HubServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
